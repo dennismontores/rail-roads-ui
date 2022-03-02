ceecf359057a28ad72574fde4429b489786dc8ac
Dennis Montores <dennis.montores@outlook.com>
2022-03-02 12:36:34 -0600

    feat(root): :building_construction: Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Paper } from '@mui/material';
+
+const Home = () => {
+    return (
+        <Paper sx={{ margin: '10px', padding: (theme) => theme.spacing(2) }}>
+            Hello it's me
+        </Paper>
+    );
+};
+
+export default Home;
