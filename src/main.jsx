01e1e9648b6c5ddaabaa2bf76983b9419a7b34ce
robertomoreno <rjmoreno@gmail.com>
2022-03-03 17:42:19 -0600

    fix an issue on dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import TrainCars from './pages/components/TrainCars'
-import ReceiverTable from './pages/components/ReceiverTable'
-import DestinationTable from './pages/components/DestinationTable'
 import DestinationWrapper from './pages/components/DestinationWrapper'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <TrainCars/>
     <DestinationWrapper/>
   </React.StrictMode>,
   document.getElementById('root')
