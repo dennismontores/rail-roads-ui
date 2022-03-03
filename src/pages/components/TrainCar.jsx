bf231575ce9d626dc166b4141f6ea7f8625fb0f0
robertomoreno <rjmoreno@gmail.com>
2022-03-02 22:47:04 -0600

    fixing a table that was having wrong column data

--- src/pages/components/TrainCar.jsx
+++ src/pages/components/TrainCar.jsx
@@ -4,8 +4,8 @@ import { TableRow, TableCell, Box, IconButton } from '@mui/material'
 export const TrainCar = ({ trainCar: { destination, name, receiver }, hasActions = true }) => {
   return (
     <TableRow key={name}>
-      <TableCell align="center">{destination}</TableCell>
       <TableCell align="center">{name}</TableCell>
+      <TableCell align="center">{destination}</TableCell>
       <TableCell align="center">{receiver}</TableCell>
       {hasActions && (
         <TableCell align="center">
